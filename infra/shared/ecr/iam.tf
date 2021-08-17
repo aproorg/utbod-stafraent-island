269bcb7460571f9a22ce8c8db6178e534435d7f5
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2021-08-17 22:13:47 +0000

    Promote to staging and prod

--- infra/shared/ecr/iam.tf
+++ infra/shared/ecr/iam.tf
@@ -53,6 +53,42 @@ data "aws_iam_policy_document" "deployer" {
       "arn:aws:s3:::thjodskra-prod-swagger-origin/*"
     ]
   }
+
+
+  statement {
+    actions = ["ecs:UpdateService"]
+    resources = [
+      "arn:aws:ecs:eu-west-1:${data.aws_caller_identity.current.account_id}:service/*/*"
+    ]
+  }
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectAcl",
+      "s3:PutObject",
+      "s3:PutObjectAcl",
+    ]
+    resources = [
+      "arn:aws:s3:::*utbod-stafraent-island-terraform-state/*/services/terraform.tfstate",
+      "arn:aws:s3:::*utbod-stafraent-island-terraform-state/services/terraform.tfstate",
+    ]
+  }
+  statement {
+    actions = [
+      "ecs:RegisterTaskDefinition",
+      "ecs:DeregisterTaskDefinition",
+    ]
+    resources = ["*"]
+  }
+  statement {
+    actions = [
+      "iam:PassRole",
+    ]
+    resources = [
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/ecs-task-execution-role-*",
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/*-task-role",
+    ]
+  }
 }
 
 resource "aws_iam_policy" "deploy" {
@@ -64,4 +100,13 @@ resource "aws_iam_policy" "deploy" {
 resource "aws_iam_user_policy_attachment" "deploy" {
   user       = aws_iam_user.deployer.name
   policy_arn = aws_iam_policy.deploy.arn
+}
+
+data "aws_iam_policy" "read_only" {
+  arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
+resource "aws_iam_user_policy_attachment" "readonly-role-policy-attach" {
+  user       = aws_iam_user.deployer.name
+  policy_arn = data.aws_iam_policy.read_only.arn
 }
