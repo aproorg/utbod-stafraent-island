3a99133642d0aad95dba8c1dd2bca4b6984545c4
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2021-08-17 21:36:56 +0000

    Presentation scripts (#33)

---
+++ presentation/4.4.2.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+created_application=$(curl -s -X 'POST' \
+  'https://dev.vmst.island.andes.cloud/api/applications' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "name": "Ólafur Darri",
+  "address": "Miðbæjargata 4",
+  "postalCode": 101,
+  "city": "Reykjavik",
+  "nationalId": "0101307789",
+  "children": [
+    {
+      "name": "Svakalegur Ólafsson",
+      "nationalId": "0101304929"
+    }
+  ],
+  "preferredJobs": [
+    {
+      "job": "Leikari"
+    },
+    {
+      "job": "Smiður"
+    }
+  ]
+}')
+
+application_id=$(echo $created_application | jq '.id' -r)
+echo "Created application with id: $application_id (PRESS ENTER TO CONTINUE)"
+
+read
+
+echo
+echo "Getting application"
+echo
+
+curl -s -X 'GET' \
+  'https://dev.vmst.island.andes.cloud/api/applications/'$application_id \
+  -H 'accept: application/json'
+
+read
+
+echo
+echo "Updating application (adding job)"
+echo
+
+curl -s -X 'PUT' \
+  'https://dev.vmst.island.andes.cloud/api/applications/'$application_id \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "name": "Ólafur Darri",
+  "address": "Miðbæjargata 4",
+  "postalCode": 101,
+  "city": "Reykjavik",
+  "nationalId": "0101307789",
+  "children": [
+    {
+      "name": "Svakalegur Ólafsson",
+      "nationalId": "0101304929"
+    }
+  ],
+  "preferredJobs": [
+    {
+      "job": "Leikari"
+    },
+    {
+      "job": "Smiður"
+    },
+    {
+      "job": "Allsherjargoði"
+    }
+  ]
+}' > /dev/null
+
+curl -s -X 'GET' \
+  'https://dev.vmst.island.andes.cloud/api/applications/'$application_id \
+  -H 'accept: application/json'
+
+read
+
+echo
+echo "Deleting application"
+echo
+
+curl -s -X 'DELETE' \
+  'https://dev.vmst.island.andes.cloud/api/applications/'$application_id \
+  -H 'accept: */*'
+curl -s -X 'GET' \
+  'https://dev.vmst.island.andes.cloud/api/applications/'$application_id \
+  -H 'accept: application/json'
+
+echo
